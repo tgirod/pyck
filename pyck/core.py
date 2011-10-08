245b7843fa3db5037da7d71c15165b28068c1c97
Tomtom <tomtom@herbesfolles.org>
2011-10-08 11:52:35 +0200

    now using properties

--- pyck/core.py
+++ pyck/core.py
@@ -2,53 +2,60 @@ class Server(object):
     """ Server is the central class taking care of the shreduling. """
     
     def __init__(self,samplerate=44100,ins=2,outs=2):
-        self.samplerate = samplerate
-        self.ins = ins
-        self.outs = outs
-        self.now = 0
-        self.q = {}
-        
+        self._samplerate = samplerate
+        self._ins = ins
+        self._outs = outs
+        self._now = 0
+        self._queue = {}
+    
+    @property
+    def samplerate(self):
+        return self._samplerate
+    
+    @property
+    def now(self):
+        return self._now
+    
     def shredule(self,time,shred):
         """ shredule the given shred at the given time """
-        if time in self.q:
-            self.q[time].append(shred)
+        if time in self._queue:
+            self._queue[time].append(shred)
         else:
-            self.q[time] = [shred]
+            self._queue[time] = [shred]
 
     def spork(self,func,*args):
         """turn a generator function into a shred and shredule it now"""
-        self.shredule(self.now,func(*args))
+        self.shredule(self._now,func(*args))
     
     def step(self):
         self.runShreds()
-        self.now += 1
+        self._now += 1
 
     def run(self):
         while True:
             self.step()
             
     def handleYield(self,s,y):
-        if type(y) == int: self.shredule(self.now+y,s)
+        if type(y) == int: self.shredule(self._now+y,s)
         # if yield returned none, reshredule s now
-        elif y == None: self.shredule(self.now,s)
+        elif y == None: self.shredule(self._now,s)
         # if y is an event, make s wait for the event
         elif isinstance(y, Event): y.shredule(s)
         
     def runShreds(self):
         """run what is shreduled now."""
         # get the list of shreds shreduled now
-        while self.now in self.q:
+        while self._now in self._queue:
             # extract the list of shreds to execute now. If a shred shredule
             # something "now", it won't appear in this list, thus the enclosing
             # while loop
-            for s in self.q.pop(self.now,[]):
+            for s in self._queue.pop(self._now,[]):
                 try:
                     y = s.next()
                     self.handleYield(s,y)
                 except StopIteration:
                     # the shred terminates
                     pass
-
 
 class Event(object):
     """ The Event class allows communication between various parts of code :
@@ -62,31 +69,31 @@ class Event(object):
     """
     def __init__(self,server):
         # queue of shreds currently listening to this event
-        self.server = server
-        self.q = []
+        self._server = server
+        self._queue = []
 
     def shredule(self,s):
         """add s at the end of the list of shreds currently listening to this
         event"""
-        self.q.append(s)
+        self._queue.append(s)
     
     def signal(self,*args):
         """send a message (args) to the first shred of the list and remove it"""
-        if len(self.q) == 0:
+        if len(self._queue) == 0:
             return
         
-        s = self.q.pop(0)
+        s = self._queue.pop(0)
         
         try:
             y = s.send(*args)
-            self.server.handleYield(s,y)
+            self._server.handleYield(s,y)
         except StopIteration:
             # the shred terminates
             pass
         
     def broadcast(self,*args):
         """send a message (args) to all the shreds listening and remove them"""
-        for s in self.q:
+        for s in self._queue:
             s.send(*args)
-        self.shreds = []
+        self._queue = []
 
