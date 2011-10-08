56459c8c86a31e6ee7f6687e5afb75f711eb01e2
Tomtom <tomtom@herbesfolles.org>
2011-10-09 00:48:13 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2
+
+from distutils.core import setup
+
+setup(name='pyck',
+      version='0.1',
+      description='Pythonized ChucK',
+      author='Thomas Girod',
+      author_email='tomtom@herbesfolles.org',
+      url='https://github.com/jiyunatori/pyck',
+      packages=['pyck','pyck.ugens']
+      )
