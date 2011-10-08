cd00aed27367d838cdb94fc3ed1c30395c180104
Tomtom <tomtom@herbesfolles.org>
2011-10-08 10:57:22 +0200

    changed test code

--- pyck/test.py
+++ pyck/test.py
@@ -1,27 +1,25 @@
-from random import random
 from core import *
+from time import sleep
+import thread
+
+def clockShred():
+    while True:
+        sleep(0.1)
+        yield 1
 
 def printShred(name,event):
     while True:
         text = yield event
         print "my name is %s and i'm printing %s" %(name,text)
 
-def genShred(event):
-    while True:
-        x = random()
-        print "sending", x
-        event.signal(x)
-        yield 3
-
 
 if __name__ == "__main__":
     s = Server()
     e = Event(s)
     
+    s.spork(clockShred)
+    s.spork(printShred,"bobby",e)
     s.spork(printShred,"steve",e)
-    s.spork(printShred,"bob",e)
-    s.spork(genShred,e)
-    
-    s.run()
+    thread.start_new_thread(s.run,())
     
     
