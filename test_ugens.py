12bd5f421d8b7fb6c9c796618e1a2a7420a6589d
Thomas Girod <girodt@gmail.com>
2011-11-28 13:54:15 +0100

    ugen example script

---
+++ test_ugens.py
@@ -0,0 +1,19 @@
+import pyck
+import pyck.ugens.osc as osc
+import pyck.ugens.filter as filter
+
+pyck.init()
+
+o1 = osc.SinOsc(freq=30)
+o2 = osc.SinOsc(freq=33,gain=0.2)
+
+pyck.connect(o1,pyck.dac)
+pyck.connect(o2,pyck.dac) 
+
+def tick():
+    pyck.now += 1
+    pyck.dac.tick()
+
+for _ in xrange(100):
+    tick()
+    print pyck.dac.input
