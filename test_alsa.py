bb14e8288c2322c952161ec462f5a2c727423a8f
Thomas Girod <girodt@gmail.com>
2011-12-15 13:28:43 +0100

    not interesting

--- test_alsa.py
+++ test_alsa.py
@@ -13,27 +13,32 @@ $> cat fifo | aplay
 and then launch this script from another terminal.
 """
 
-pyck.srate = 8000
+pyck.srate = 44100
 
-x = osc.SinOsc(freq=1000)
+x = osc.SinOsc(freq=1000,gain=0.5)
 pyck.dac.addSource(x)
 
 def randomfreq(osc):
     while True:
-        osc.freq = uniform(100,1000)
-        yield 1000
+        osc.freq = uniform(500,1100)
+        yield pyck.second(0.2)
 
 pyck.spork(randomfreq(x))
 
 pipe = open('fifo','wb')
 
+def convert(v):
+    v = int(v*32768)
+    v = min(v,32767)
+    v = max(v,-32768)
+    return v
+
 def tick():
     pyck.tick()
-    v = pyck.dac.input[0]
-    v = (v+1)/2 * 256
-    v = min(v,255)
-    v = max(v,0)
-    pipe.write(struct.pack('@B',v))
+    v0 = convert(pyck.dac.input[0])
+    v1 = convert(pyck.dac.input[1])
+    pipe.write(struct.pack('<hh',v0,v1))
 
-for _ in range(pyck.srate*5):
-    tick()
+if __name__ == "__main__":
+    for _ in range(pyck.srate*5):
+        tick()
